3dc6415007697f571a7b1a50c8a784cdb61a9995
Fahd <fahd.pixel@gmail.com>
2023-06-01 12:41:48 +0530

    Initial SetUp

---
+++ components/Nav.jsx
@@ -0,0 +1,26 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+
+const Nav = () => {
+  return (
+    <nav className="w-full flex-between mb-16 pt-3">
+      <Link className="flex flex-center gap-2" href="/">
+        <Image
+          src="/assets/images/logo.svg"
+          alt="Promptopia Logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+        <p className="logo_text">Promptopia</p>
+      </Link>
+      
+    </nav>
+  );
+};
+
+export default Nav;
